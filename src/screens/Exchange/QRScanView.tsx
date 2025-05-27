7b33e858074ce767d3e1a3e131a93dab0ded8f52
Yebin Kim <rladpqls07@gmail.com>
2025-05-28 04:46:46 +0900

    fix: 카메라 중복 스캔 방지 처리 #25

--- src/screens/Exchange/QRScanView.tsx
+++ src/screens/Exchange/QRScanView.tsx
@@ -1,15 +1,13 @@
 import { Text, TouchableOpacity, View, StyleSheet, Alert } from 'react-native';
 import { CameraView, useCameraPermissions } from 'expo-camera';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 export default function QRScanView({ navigation }: any) {
-	const [scanned, setScanned] = useState(false);
+	const scannedRef = useRef(false);
 	const [permission, requestPermission] = useCameraPermissions();
 
-	/** 바코드 스캔 초기화 함수 */
-	const handleResetScan = () => {
-		setScanned(false);
-	};
+	/** 바코드 스캔 상태 초기화 함수 */
+	const handleResetScan = () => (scannedRef.current = false);
 
 	/** 스캔한 명함을 저장하고 해당 명함 상세 페이지로 이동하는 함수 */
 	const handleSaveCard = (qrCardId: number) => {
@@ -20,23 +18,23 @@ export default function QRScanView({ navigation }: any) {
 	};
 
 	/** 바코드 스캔 데이터 저장 함수 */
-	const handleBarcodeScan = React.useCallback(
-		({ data }: { data: string }) => {
-			// 스캔된 상태 업데이트
-			setScanned(true);
+	const handleBarcodeScan = ({ data }: { data: string }) => {
+		if (scannedRef.current) return;
+		scannedRef.current = true;
 
-			// 데이터 유효성 검사
-			if (!data.startsWith('scannect://')) {
-				Alert.alert('유효하지 않은 QR 코드입니다.');
-				setScanned(false);
-				return;
-			}
-
+		if (!data.startsWith('scannect://')) {
+			Alert.alert('유효하지 않은 QR 코드입니다.', 'SCANNECT 명함 QR코드를 인식해주세요!', [
+				{
+					text: '확인',
+					onPress: handleResetScan,
+				},
+			]);
+		} else {
 			// 링크 url에서 cardId 정보 get
 			const parsedURL = data.split('/');
 			const scannedCardId = parseInt(parsedURL[parsedURL.length - 1]);
 
-			console.log(scanned, scannedCardId);
+			console.log(scannedRef.current, scannedCardId);
 
 			Alert.alert(`명함 스캔 완료! id: ${scannedCardId}`, '스캔한 명함을 저장합니다...', [
 				{
@@ -45,9 +43,8 @@ export default function QRScanView({ navigation }: any) {
 				},
 				{ text: '취소', onPress: handleResetScan },
 			]);
-		},
-		[scanned]
-	);
+		}
+	};
 
 	useEffect(() => {
 		if (!permission) {
@@ -68,15 +65,13 @@ export default function QRScanView({ navigation }: any) {
 
 	return (
 		<View style={styles.container}>
-			{!scanned && (
-				<CameraView
-					style={styles.scanner}
-					onBarcodeScanned={handleBarcodeScan}
-					barcodeScannerSettings={{
-						barcodeTypes: ['qr'],
-					}}
-				/>
-			)}
+			<CameraView
+				style={styles.scanner}
+				onBarcodeScanned={handleBarcodeScan}
+				barcodeScannerSettings={{
+					barcodeTypes: ['qr'],
+				}}
+			/>
 		</View>
 	);
 }
