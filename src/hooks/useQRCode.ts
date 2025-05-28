d4eec0b263c1a758c6188dedaab5cc0f36a55528
Yebin Kim <rladpqls07@gmail.com>
2025-05-28 16:27:34 +0900

    refactor: useQRCode 훅 관련 코드 리팩토링 #25

--- src/hooks/useQRCode.ts
+++ src/hooks/useQRCode.ts
@@ -1,12 +1,16 @@
+import { useNavigation } from '@react-navigation/native';
 import * as FileSystem from 'expo-file-system';
 import * as Sharing from 'expo-sharing';
 import { useRef } from 'react';
+import { Alert } from 'react-native';
 
 export function useQRCode() {
 	const qrRef = useRef<any>(null);
+	const scannedRef = useRef(false);
+	const navigation = useNavigation<any>();
 
-	const onQRShare = async () => {
-		// qr코드 BASE64 인코딩 후 파일 공유
+	/** qr코드 Base64 인코딩 후 파일 저장 및 외부로 공유하는 함수 */
+	const shareQR = async () => {
 		console.log('qr 공유 중...');
 		qrRef.current?.toDataURL(async (data: string) => {
 			const filename = FileSystem.documentDirectory + 'scannect_qrcode.png';
@@ -21,5 +25,48 @@ export function useQRCode() {
 			}
 		});
 	};
-	return { qrRef, onQRShare };
+
+	/** 바코드 스캔 상태 초기화 함수 */
+	const resetScan = () => (scannedRef.current = false);
+
+	/** 스캔한 명함을 저장하고 해당 명함 상세 페이지로 이동하는 함수 */
+	const saveCard = (qrCardId: number) => {
+		console.log(qrCardId, '저장 중...');
+		/* 명함 저장 */
+		/* 네비게이션 */
+		navigation.pop();
+		navigation.navigate('CardListTab', { screen: 'CardDetail', params: { cardId: qrCardId } });
+	};
+
+	/** 바코드 스캔 데이터 저장 함수 */
+	const scanQR = (data: string) => {
+		if (scannedRef.current) return;
+		scannedRef.current = true;
+
+		// qr코드 유효성 검사
+		if (!data.startsWith('scannect://')) {
+			Alert.alert('유효하지 않은 QR 코드입니다.', 'SCANNECT 명함 QR코드를 인식해주세요!', [
+				{
+					text: '확인',
+					onPress: resetScan,
+				},
+			]);
+		} else {
+			// 링크 url에서 cardId 정보 get
+			const parsedURL = data.split('/');
+			const scannedCardId = parseInt(parsedURL[parsedURL.length - 1]);
+
+			console.log(scannedRef.current, scannedCardId);
+
+			Alert.alert(`명함 스캔 완료! id: ${scannedCardId}`, '스캔한 명함을 저장합니다...', [
+				{
+					text: '저장',
+					onPress: () => saveCard(scannedCardId),
+				},
+				{ text: '취소', onPress: resetScan },
+			]);
+		}
+	};
+
+	return { qrRef, shareQR, scanQR };
 }
--- src/screens/Exchange/QRScanView.tsx
+++ src/screens/Exchange/QRScanView.tsx
@@ -1,49 +1,16 @@
-import { Text, TouchableOpacity, View, StyleSheet, Alert } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import { CameraView, useCameraPermissions } from 'expo-camera';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 
-export default function QRScanView({ navigation }: any) {
-	const scannedRef = useRef(false);
+import { useQRCode } from '../../hooks/useQRCode';
+import CommonButton from '../../components/CommonButton';
+
+export default function QRScanView() {
+	const { scanQR } = useQRCode();
 	const [permission, requestPermission] = useCameraPermissions();
 
-	/** 바코드 스캔 상태 초기화 함수 */
-	const handleResetScan = () => (scannedRef.current = false);
-
-	/** 스캔한 명함을 저장하고 해당 명함 상세 페이지로 이동하는 함수 */
-	const handleSaveCard = (qrCardId: number) => {
-		console.log(qrCardId, '저장 중...');
-		/* 명함 저장 */
-		/* 네비게이션 */
-		navigation.navigate('CardListTab', { screen: 'CardDetail', params: { cardId: qrCardId } });
-	};
-
-	/** 바코드 스캔 데이터 저장 함수 */
-	const handleBarcodeScan = ({ data }: { data: string }) => {
-		if (scannedRef.current) return;
-		scannedRef.current = true;
-
-		if (!data.startsWith('scannect://')) {
-			Alert.alert('유효하지 않은 QR 코드입니다.', 'SCANNECT 명함 QR코드를 인식해주세요!', [
-				{
-					text: '확인',
-					onPress: handleResetScan,
-				},
-			]);
-		} else {
-			// 링크 url에서 cardId 정보 get
-			const parsedURL = data.split('/');
-			const scannedCardId = parseInt(parsedURL[parsedURL.length - 1]);
-
-			console.log(scannedRef.current, scannedCardId);
-
-			Alert.alert(`명함 스캔 완료! id: ${scannedCardId}`, '스캔한 명함을 저장합니다...', [
-				{
-					text: '저장',
-					onPress: () => handleSaveCard(scannedCardId),
-				},
-				{ text: '취소', onPress: handleResetScan },
-			]);
-		}
+	const handleBarcodeScanned = ({ data }: { data: string }) => {
+		scanQR(data);
 	};
 
 	useEffect(() => {
@@ -56,9 +23,7 @@ export default function QRScanView({ navigation }: any) {
 		return (
 			<View style={styles.container}>
 				<Text>카메라 사용 권한이 필요합니다.</Text>
-				<TouchableOpacity onPress={requestPermission}>
-					<Text>권한 요청</Text>
-				</TouchableOpacity>
+				<CommonButton title="권한 요청" onPress={requestPermission} />
 			</View>
 		);
 	}
@@ -67,7 +32,7 @@ export default function QRScanView({ navigation }: any) {
 		<View style={styles.container}>
 			<CameraView
 				style={styles.scanner}
-				onBarcodeScanned={handleBarcodeScan}
+				onBarcodeScanned={handleBarcodeScanned}
 				barcodeScannerSettings={{
 					barcodeTypes: ['qr'],
 				}}
