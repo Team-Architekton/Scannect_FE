d9b889d4923a869b5682b5688cbfe7880e51706c
Yebin Kim <rladpqls07@gmail.com>
2025-05-31 00:45:37 +0900

    refactor: ProfileInfo 컴포넌트 파일 정리 #30

--- src/screens/CardList/CardDetailView.tsx
+++ src/screens/CardList/CardDetailView.tsx
@@ -2,28 +2,22 @@ import { KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
 import { useEffect, useRef } from 'react';
 
 import ScreenContainer from '../../components/ScreenContainer';
+import CardBottomSheet from '../../components/cardList/elements/CardBottomSheet';
 import Header from '../../components/cardDetail/Header';
 import MemoInput from '../../components/cardDetail/Memo';
-import CardPreview from '../../components/mypage/CardPreview';
-import Contacts from '../../components/cardDetail/Contacts';
-import CardBottomSheet from '../../components/cardList/elements/CardBottomSheet';
-import ProfileIntro from './../../components/mypage/elements/ProfileIntro';
-import InfoSection from '../../components/mypage/elements/InfoSection';
+import ProfileInfo from '../../components/cardDetail/ProfileInfo';
 import { useCardStore } from '../../store/cardStore';
-import { useCardForm } from '../../hooks/useCardForm';
 
 export default function CardDetailView({ navigation, route }: any) {
 	const scrollRef = useRef<ScrollView>(null);
 	const { cardId } = route.params;
 	const card = useCardStore(state => state.cardList.find(c => c.id === cardId));
-	const { form, errors } = useCardForm({ ...card });
 
 	useEffect(() => {
 		if (!card) navigation.goBack();
 	}, [card]);
 
 	const handleFocus = () => {
-		// 키보드 올라올 때 화면 맨 아래로 스크롤
 		setTimeout(() => {
 			scrollRef.current?.scrollToEnd({ animated: true });
 		}, 300);
@@ -44,22 +38,7 @@ export default function CardDetailView({ navigation, route }: any) {
 						favorite={card.favorite}
 						isActive={card.isActive}
 					/>
-					<CardPreview selectedCard={card} />
-					<Contacts phone={card.phoneNum} email={card.email} />
-					<ProfileIntro
-						content={card.content ?? ''}
-						imgUrl={card.imgUrl ?? null}
-						onChangeContent={text => {}}
-						onChangeImg={uri => {}}
-						isEditing={false}
-					/>
-					<InfoSection
-						form={form}
-						errors={errors}
-						isEditing={false}
-						handleChange={(key, value) => {}}
-						validateField={(key, value) => {}}
-					/>
+					<ProfileInfo card={card} />
 					<MemoInput cardId={card.id} memo={card.memo} onFocus={handleFocus} />
 				</ScrollView>
 			</KeyboardAvoidingView>
