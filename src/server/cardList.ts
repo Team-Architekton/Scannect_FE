de85ffe21a88cb228b68e8bf99a3804998c4e0f1
Yebin Kim <rladpqls07@gmail.com>
2025-06-05 01:44:13 +0900

    feat: 타명함 api 호출 함수 생성 #37

---
+++ src/server/cardList.ts
@@ -0,0 +1,26 @@
+import { getUserId } from '../utils/authStorage';
+import { httpClient } from './http';
+
+/** 타유저 명함 전체 리스트 조회 API */
+export const getCardList = async () => {
+	const userId = await getUserId();
+	if (!userId) return;
+
+	const { data } = await httpClient.get(`/card-list/user/${userId}`);
+	console.log(data);
+};
+
+/** 타유저 명함 숨김 상태 업데이트 API */
+export const hideCard = async (cardId: number, newStatus: boolean) => {
+	await httpClient.patch(`/card-list/${cardId}/active?isActive=${newStatus}`);
+};
+
+/** 타유저 명함 중요 표시 업데이트 API */
+export const starrCard = async (cardId: number, newStatus: boolean) => {
+	await httpClient.patch(`/card-list/${cardId}/favorite?favorite=${newStatus}`);
+};
+
+/** 타유저 명함 삭제 API */
+export const deleteCard = async (cardId: number) => {
+	await httpClient.delete(`/card-list/${cardId}`);
+};
