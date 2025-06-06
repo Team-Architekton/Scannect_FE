fa7deceea4f194f0866bc57e2a33cb275eeec916
Yebin Kim <rladpqls07@gmail.com>
2025-06-06 21:29:16 +0900

    fix: api 호출 함수 수정 #37

--- src/server/cardList.ts
+++ src/server/cardList.ts
@@ -6,22 +6,18 @@ export const getCards = async () => {
 	const userId = await getUserId();
 	if (!userId) return; // id: tester, password: 123456
 
-	try {
-		const { data } = await httpClient.get(`/card-list/user/${userId}`);
-		console.log('명함 리스트 조회 api 호출 res', data);
-		return data.data;
-	} catch (e) {
-		throw e;
-	}
+	const { data } = await httpClient.get(`/card-list/user/${userId}`);
+	console.log('명함 리스트 조회 api 호출 res', data);
+	return data.data;
 };
 
 /** 타유저 명함 숨김 상태 업데이트 API */
-export const hideCard = async (cardId: number, newStatus: boolean) => {
+export const updateHiddenCard = async (cardId: number, newStatus: boolean) => {
 	await httpClient.patch(`/card-list/${cardId}/active?isActive=${newStatus}`);
 };
 
 /** 타유저 명함 중요 표시 업데이트 API */
-export const starrCard = async (cardId: number, newStatus: boolean) => {
+export const updateFavCard = async (cardId: number, newStatus: boolean) => {
 	await httpClient.patch(`/card-list/${cardId}/favorite?favorite=${newStatus}`);
 };
 
