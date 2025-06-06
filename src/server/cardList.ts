49ab5b5d9cc9ca8f40b44d643ca2f22ab59e4b41
Yebin Kim <rladpqls07@gmail.com>
2025-06-07 00:54:42 +0900

    fix: 명함 수정, 삭제 api 호출 함수 로직 수정 #37

--- src/server/cardList.ts
+++ src/server/cardList.ts
@@ -7,21 +7,20 @@ export const getCards = async () => {
 	if (!userId) return; // id: tester, password: 123456
 
 	const { data } = await httpClient.get(`/card-list/user/${userId}`);
-	console.log('명함 리스트 조회 api 호출 res', data);
 	return data.data;
 };
 
 /** 타유저 명함 숨김 상태 업데이트 API */
-export const updateHiddenCard = async (cardId: number, newStatus: boolean) => {
-	await httpClient.patch(`/card-list/${cardId}/active?isActive=${newStatus}`);
+export const updateIsActive = async (tableId: number, newStatus: boolean) => {
+	await httpClient.patch(`/card-list/${tableId}/active?isActive=${newStatus}`);
 };
 
 /** 타유저 명함 중요 표시 업데이트 API */
-export const updateFavCard = async (cardId: number, newStatus: boolean) => {
-	await httpClient.patch(`/card-list/${cardId}/favorite?favorite=${newStatus}`);
+export const updateFavorite = async (tableId: number, newStatus: boolean) => {
+	await httpClient.patch(`/card-list/${tableId}/favorite?favorite=${newStatus}`);
 };
 
 /** 타유저 명함 삭제 API */
-export const deleteCard = async (cardId: number) => {
-	await httpClient.delete(`/card-list/${cardId}`);
+export const deleteCard = async (tableId: number) => {
+	await httpClient.delete(`/card-list/${tableId}`);
 };
