3d75ddaa38774e325e20e5863aabfa6694c753e7
Yebin Kim <rladpqls07@gmail.com>
2025-06-08 16:38:15 +0900

    fix: qr 공유, 스캔 및 저장 비즈니스 로직 정리 #45

--- src/screens/Exchange/QRScanView.tsx
+++ src/screens/Exchange/QRScanView.tsx
@@ -1,16 +1,49 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Alert } from 'react-native';
 import { CameraView, useCameraPermissions } from 'expo-camera';
 import React, { useEffect } from 'react';
 
 import { useQRCode } from '../../hooks/useQRCode';
 import CommonButton from '../../components/CommonButton';
 
-export default function QRScanView() {
-	const { scanQR } = useQRCode();
+export default function QRScanView({ navigation }: any) {
+	const { scanQR, resetScan, saveQRCard } = useQRCode();
 	const [permission, requestPermission] = useCameraPermissions();
 
+	const handleSaveQRCard = async (cardId: number) => {
+		const success = await saveQRCard(cardId);
+		if (!success) {
+			Alert.alert('처리 실패', '잠시 후 다시 시도해주세요.');
+			resetScan();
+		} else {
+			/* 네비게이션 */
+			navigation.pop();
+			navigation.navigate('CardListTab', { screen: 'CardDetail', params: { cardId } });
+		}
+	};
+
 	const handleBarcodeScanned = ({ data }: { data: string }) => {
-		scanQR(data);
+		const { dupScanned, validate } = scanQR(data);
+		if (dupScanned) return;
+		if (!validate) {
+			Alert.alert('유효하지 않은 QR 코드입니다.', '정확한 명함 QR을 인식해주세요!', [
+				{
+					text: '확인',
+					onPress: resetScan,
+				},
+			]);
+		} else {
+			// 링크 url에서 cardId 정보 get
+			const paths = new URL(data).pathname.split('/');
+			const scannedCardId = Number(paths.pop());
+
+			Alert.alert('명함 스캔 완료!', '스캔한 명함을 저장합니다...', [
+				{
+					text: '저장',
+					onPress: () => handleSaveQRCard(scannedCardId),
+				},
+				{ text: '취소', onPress: resetScan },
+			]);
+		}
 	};
 
 	useEffect(() => {
