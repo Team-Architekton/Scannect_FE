c9b598b3a7d16c2e8fe5d470dfdad9c5bc0879af
Yebin Kim <rladpqls07@gmail.com>
2025-06-11 15:33:25 +0900

    fix: ICardItem 속성명 수정 #59

--- src/model/cardItem.ts
+++ src/model/cardItem.ts
@@ -5,19 +5,18 @@ export interface ICardItem {
 	cardName: string; // 명함 이름
 	nickname: string; // 유저 닉네임
 	belongTo: string; // 소속
-	department: string; // 부서
-	position: string; // 직급
+	department?: string; // 부서
+	position: string; // 직책
 	industry: string; // 업종
 	job: string; // 직무
 
 	memo: string;
 	favorite: boolean;
 	isActive: boolean;
-	createdAt: string;
 
-	phoneNum?: string;
-	email?: string;
-	urlList?: string[];
+	phoneNum: string;
+	email: string;
+	url?: string;
 	companyTel?: string;
 
 	imgUrl?: string | null;
